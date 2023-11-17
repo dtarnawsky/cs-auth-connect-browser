07f0cf58cb80abbf262dc7a4e4aae2b1be403647
damian.ionic <damian@ionic.io>
2023-11-17 09:37:31 -0800

    Initial

---
+++ capacitor.config.ts
@@ -0,0 +1,14 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.conferences.eas2021',
+  appName: 'EAS 21',
+  webDir: 'www',
+  // cordova: {
+  //   preferences: {
+  //     "DisableDeploy": "true"
+  //   }
+  // }
+};
+
+export default config;
